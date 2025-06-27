bd41f95ed41a0ae1229d9e12c8794674b2bee4d1
ShigemotoTsubasa <shigemoto_tsubasa@retail-ai.jp>
2025-06-27 15:19:21 +0900

    テキストを+1に変更

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _CounterState extends State<Counter> {
               count++;
             });
           },
-          child: const Text('Increment'),
+          child: const Text('+1'),
         ),
       ],
     );
