a5c0f918ec6217c99fd52c81c4477e63a32196a0
ShigemotoTsubasa <shigemoto_tsubasa@retail-ai.jp>
2025-06-30 17:15:26 +0900

    mobile_Scannerの追加 バーコードの読み取りの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,64 @@
+import 'package:first_app/BarcodeReader.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String scannedValue = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('QRコードリーダー')),
+      body: Center(
+        child: Text(
+          scannedValue.isEmpty ? 'バーコードをスキャンしてください' : scannedValue,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: 'QRコードリーダー',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) async {
+          if (index == 1) {
+            final result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const BarcodeReaderPage(),
+              ),
+            );
+            if (result != null) {
+              setState(() {
+                scannedValue = result;
+              });
+            }
+          }
+        },
+      ),
     );
   }
 }
