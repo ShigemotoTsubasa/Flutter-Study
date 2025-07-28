1d51d477bf74cc2f4ecd593b72b63fb556a429c9
ShigemotoTsubasa <shigemoto_tsubasa@retail-ai.jp>
2025-07-28 17:39:42 +0900

    コードレビューの修正

--- lib/Dialog/notification_manager.dart
+++ lib/Dialog/notification_manager.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 
 class NotificationManager {
-  void showError(String errorMessage, BuildContext context) {
+  static void showError(String errorMessage, BuildContext context) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(errorMessage), backgroundColor: Colors.red),
     );
